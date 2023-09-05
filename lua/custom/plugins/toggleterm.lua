f00568e1d9277126d25787d37ec61598139381db
ducng99 <tom@ducng.dev>
2023-09-05 12:26:45 +1200

    toggle terminal in all modes

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -15,12 +15,14 @@ return {
         function()
           vim.cmd('ToggleTerm direction=vertical size=' .. vim.o.columns * 0.4)
         end,
+        mode = { 'n', 'v', 'i', 't' },
       },
       {
         '<M-h>',
         function()
           vim.cmd('ToggleTerm direction=horizontal size=' .. vim.o.lines * 0.4)
         end,
+        mode = { 'n', 'v', 'i', 't' },
       },
     },
   },
